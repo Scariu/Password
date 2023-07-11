5c4b6e9fde9cb7b0e48a40412895aff83125e329
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-10 22:06:08 -0400

    Se crea método evaluarClave() en clase Presentador.

--- app/src/main/java/com/example/password/Presentador.java
+++ app/src/main/java/com/example/password/Presentador.java
@@ -1,4 +1,17 @@
 package com.example.password;
 
 public class Presentador {
+    protected Modelo modelo;
+    public void evaluarClave(String password){
+        int nivelFortaleza = modelo.validarClave(password);
+        if (nivelFortaleza == Modelo.WEAK) {
+
+        } else if (nivelFortaleza == Modelo.MEDIUM) {
+
+        } else if (nivelFortaleza == Modelo.STRONG) {
+
+        }else {
+
+        }
+    }
 }
