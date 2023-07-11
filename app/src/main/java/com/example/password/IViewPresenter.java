7f9b868c39e0516bc7bebb037a5a4747243cc0a6
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-10 22:16:52 -0400

    Se vincula Presenter a interface y se crea método showError() en interface.

--- app/src/main/java/com/example/password/IViewPresenter.java
+++ app/src/main/java/com/example/password/IViewPresenter.java
@@ -8,4 +8,6 @@ public interface IViewPresenter {
 
    public void showStrong();
 
+   public void showError();
+
 }
--- app/src/main/java/com/example/password/Presentador.java
+++ app/src/main/java/com/example/password/Presentador.java
@@ -1,17 +1,28 @@
 package com.example.password;
 
 public class Presentador {
-    protected Modelo modelo;
-    public void evaluarClave(String password){
+    private IViewPresenter iViewPresenter;
+    private Modelo modelo;
+    public Presentador(IViewPresenter iViewPresenter){
+        this.iViewPresenter = iViewPresenter;
+
+        modelo = new Modelo();
+    }
+
+    //Se enlaza Presentador a Modelo
+    public void evaluarClave(String password) {
         int nivelFortaleza = modelo.validarClave(password);
         if (nivelFortaleza == Modelo.WEAK) {
+            this.iViewPresenter.showWeak();
 
         } else if (nivelFortaleza == Modelo.MEDIUM) {
+            this.iViewPresenter.showMedium();
 
         } else if (nivelFortaleza == Modelo.STRONG) {
+            this.iViewPresenter.showStrong();
 
-        }else {
-
+        } else {
+            this.iViewPresenter.showError();
         }
     }
 }
